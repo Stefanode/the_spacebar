0d81f75e87e4944d49800391168eb85b851e675d
Steef <stefanodegenkamp@gmail.com>
2018-04-18 10:12:23 +0200

    Article controller rebuild

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -21,9 +22,8 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            "Future page to show cool article: %s",
-            $slug
-        ));
+        return $this->render("article/show.html.twig", [
+            "title" => ucwords(str_replace('-', ' ', $slug)),
+        ]);
     }
 }
