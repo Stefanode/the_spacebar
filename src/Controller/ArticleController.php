04081564194aebd835ead69e64c89737d7874ea8
Steef <stefanodegenkamp@gmail.com>
2018-04-19 13:29:08 +0200

    Code updated with new code from tutorial

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
-
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
